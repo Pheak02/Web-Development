8008c6b9f587c4e9b83c1f513a5a78504cc5af1b
Pheak02 <saingsopheak02@gmail.com>
2023-09-14 17:23:08 +0700

    159. String Template Literals -SUPER USEFUL

---
+++ index.js
@@ -0,0 +1,7 @@
+let product = 'Angkor Milk';
+let price = 3.99;
+let qty=5;
+let order= "you bought" + qty + " " + product + ". Total is: " + price*qty;
+
+let literal = `hello ${1+2}`
+let value= `you bought ${qty} ${product}.Total is: ${price*qty}`
