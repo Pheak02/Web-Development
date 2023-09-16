ff4e8ab8fc6c3d6420e9316fedc693bfd52d200a
Pheak02 <saingsopheak02@gmail.com>
2023-09-16 14:43:13 +0700

    add bcrypt password using hash

--- BcryptDemo/index.js
+++ BcryptDemo/index.js
@@ -1,7 +1,19 @@
 const bcrypt = require('bcrypt');
 
-const hashPassword = async () => {
+const hashPassword = async (pw) => {
     const salt = await bcrypt.genSalt(10); //salt indicate how many round, bcrypt need to get into
+    const hash = await bcrypt.hash(pw, salt);
     console.log(salt);
+    console.log(hash);
 }
-hashPassword();
+
+const login =async (pw, hashedPw) => {
+    const result = await bcrypt.compare(pw, hashedPw);
+    if(result){
+        console.log("Logged you in! successful match!")
+    }else{
+        console.log("INCORRECT!")
+    }
+}
+// hashPassword('monkey');
+login('monkey', '$2b$10$RelD/rdx1HxXSmziCArkrOOTc4M2Q4AlqhQObeOMgsMK8/Xg9W886')
