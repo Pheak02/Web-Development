d938b4e31c978ba7d407d0040a95fe0dc1542628
Pheak02 <saingsopheak02@gmail.com>
2024-05-05 23:06:49 +0700

    php basic - working with string

--- PHP/www/site.php
+++ PHP/www/site.php
@@ -10,12 +10,23 @@
     // VAR
     $characterName = "Sophie";
     $characterAge = "20";
-
+    echo("<hr>");
     // WRITING HTML
     echo("There is a women named $characterName<br>");
     echo ("She was nearly $characterAge years old <br>");
     echo ("She really like here name but was doubt her life's choice<br>");
     echo ("But don't like turning $characterAge<br>");
+
+    //WORKING W STRING
+    echo("<hr>");
+    $phrase = "Hello Miller Universe!";
+    echo strtolower($phrase);
+    echo strtoupper($phrase);
+    echo strlen($phrase);
+    echo $phrase[0];//print out specific index of str
+    echo("<br>");
+    echo str_replace("Miller", "Unknown", $phrase);
+    echo substr($phrase, 8,10);//to echo specific substring
     ?>
 </body>
 </html>
