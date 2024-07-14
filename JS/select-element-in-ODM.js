33eb7359c93a5a761a70e9f7d2430016303379f9
Pheak02 <saingsopheak02@gmail.com>
2024-07-14 22:59:54 +0700

    select element in the DOM

---
+++ JS/select-element-in-ODM.js
@@ -0,0 +1,20 @@
+//DOm Manipulation
+
+//GetElementById()
+const title = document.getElementById("main-heading");
+console.log(title);
+
+//GetElementByClassName()
+
+//getElementsByTagName()
+const listItems = document.getElementsByTagName('li');
+console.log(listItems)
+
+//querySelector()
+const queryDiv  = document.querySelector('div')
+console.log(queryDiv)
+
+//querySelectorAll()
+
+const queryAll  = document.querySelectorAll('div')
+console.log(queryAll)
